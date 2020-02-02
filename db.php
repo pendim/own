778f21ce6a213f2560ba075855385410422ca7c8
Thomas Wich <thomas@Thomass-MBP.fritz.box>
2020-02-02 20:34:54 +0100

    commit

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+
+//Alle Error anzeigen lassen
+error_reporting(E_ALL);
+
+define('MYSQL_HOST','192.168.188.36');
+define('MYSQL_BENUTZER', 'UserRead');
+define('MYSQL_KENNWORT','bitfahrzentraleBYTommy');
+define('MYSQL_DATENBANK','bitfahrzentrale');
+
+?>
